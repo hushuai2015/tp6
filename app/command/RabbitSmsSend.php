5411be7a43de708a76260e2074dfdffeb074d53b
年华里等梦搁浅 <793564147@qq.com>
2021-07-30 14:05:41 +0800

    update

---
+++ app/command/RabbitSmsSend.php
@@ -0,0 +1,25 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\command;
+use hs\rabbitmq\facade\RabbitFacade;
+use think\console\Command;
+use think\console\Input;
+use think\console\Output;
+
+class RabbitSmsSend extends Command
+{
+
+    protected function configure()
+    {
+        // 指令配置
+        $this->setName('rabbitSmsSend')
+            ->setDescription('消费队列【rabbitSmsSend】 ');
+    }
+
+    protected function execute(Input $input, Output $output)
+    {
+        RabbitFacade::consumer('sms_send');
+    }
+
+}
