867fa0f6b9a17b2b03ced9838cff6dc00aad867b
年华里等梦搁浅 <793564147@qq.com>
2021-07-30 14:13:51 +0800

    update

---
+++ extend/mq/SmsSend.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace mq;
+
+use hs\rabbitmq\ConsumerHandler;
+
+class SmsSend
+{
+    use ConsumerHandler;
+
+
+    /**
+     * @param array $param
+     * @param int $count 执行次数
+     * @return bool
+     */
+    public function test(array $param,int $count): bool
+    {
+        $data = $this->init($param);
+        echo json_encode($data) . "\n" ;
+        sleep(1);
+        return $this->release()->error();
+    }
+    
+}
